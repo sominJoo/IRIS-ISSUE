1ff49ae2bc49894470dbae0af214f1b74eb57395
somin <js06m13@gmail.com>
2021-07-09 16:49:43 +0900

    Server test

---
+++ src/main/java/com/iris/irisissue/vo/Issue.java
@@ -0,0 +1,12 @@
+package com.iris.irisissue.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Issue {
+    private int total;
+}
---
+++ src/test/java/com/iris/irisissue/IrisIssueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iris.irisissue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrisIssueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
