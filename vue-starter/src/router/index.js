a7eb1605c4c8607128bd757167a923c0fb835c1b
somin <js06m13@gmail.com>
2021-07-13 16:12:01 +0900

    Chart data 완료

---
+++ vue-starter/src/router/index.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import UserChart from '../views/UserChart.vue'
+import AssigneeChart from '../views/AssigneeChart.vue'
+import DayChart from '../views/DayChart.vue'
+import IssuesList from '../views/IssuesList.vue'
+import Header from '../views/Header.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    components: {
+      userChart: UserChart,
+      header: Header,
+      assigneeChart : AssigneeChart,
+      dayChart : DayChart,
+      issuesList: IssuesList
+    }
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
